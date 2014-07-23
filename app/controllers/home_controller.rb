3ebd5821701fe9b5b2626ae23df742552591c047
Mario Andrés Correa <a@macool.me>
2014-07-23 09:22:50 -0500

    minor enhances

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    redirect_to users_path if current_user && current_user.admin
+    redirect_to actores_path if current_user && current_user.admin
   end
 end
