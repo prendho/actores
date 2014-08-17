f6caca62b1189da62f17bfc375ecc26ced1bd46d
Mario Andrés Correa <a@macool.me>
2014-08-17 12:24:35 -0500

    iniciativas support

--- app/controllers/preguntas_controller.rb
+++ app/controllers/preguntas_controller.rb
@@ -1,4 +1,6 @@
 class PreguntasController < ApplicationController
+  attr_reader :actor
+
   before_action :require_login
   before_action :require_admin
   before_action :find_actor
@@ -21,4 +23,23 @@ class PreguntasController < ApplicationController
     @grupo_preguntas ||= @pregunta.grupo_preguntas
   end
   helper_method :grupo_preguntas
+
+  def iniciativa
+    @iniciativa ||= Iniciativa.find(params[:iniciativa_id]) if params[:iniciativa_id].present?
+  end
+  helper_method :iniciativa
+
+  def resource
+    iniciativa or actor
+  end
+  helper_method :resource
+
+  def pregunta_path
+    if iniciativa.present?
+      actor_iniciativa_path(@actor, iniciativa, preguntas: grupo_preguntas.id)
+    else
+      actor_path(@actor, preguntas: grupo_preguntas.id)
+    end
+  end
+  helper_method :pregunta_path
 end
